1e38ddde03ea841cb47c4bb8a7a4009ac5141eed
cackharot <cackharot@gmail.com>
2014-10-02 20:40:11 +0530

    updated email template img

--- src/foodbeazt/run_uwsgi.sh
+++ src/foodbeazt/run_uwsgi.sh
@@ -1,1 +1,2 @@
-sudo uwsgi -s /tmp/uwsgi.sock -w wsgi:app --chown=www-data:www-data
+#!/bin/sh
+sudo uwsgi -s /tmp/uwsgi.sock -w wsgi:app --chown=www-data:www-data --logto2 /var/log/nginx/uwsgi_log.log
