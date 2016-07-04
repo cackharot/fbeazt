5988160a69c4804b81deef388cd0f0835d4940da
cackharot <cackharot@gmail.com>
2016-07-04 21:12:03 +0530

    updated bundling logic

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -3,6 +3,9 @@ const bower = require('gulp-bower');
 const sass = require('gulp-sass');
 const gulp = require('gulp');
 const del = require('del');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const htmlreplace = require('gulp-html-replace');
 const typescript = require('gulp-typescript');
 const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
@@ -10,16 +13,23 @@ const tslint = require('gulp-tslint');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const tsconfig = require('tsconfig-glob');
+const addsrc = require('gulp-add-src');
+const Builder = require('systemjs-builder');
+
+var tsProject = typescript.createProject('tsconfig.json', {
+  typescript: require('typescript'),
+  outFile: 'app.js'
+});
 
 // clean the contents of the distribution directory
 gulp.task('clean', function () {
   return del('dist/**/*');
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', ['clean'], function () {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('sass:watch', function () {
@@ -33,34 +43,27 @@ gulp.task('bower', function() {
 
 // copy static assets - i.e. non TypeScript compiled source
 gulp.task('copy:assets', ['clean'], function() {
-  return gulp.src(['app/**/*', 'index.html',
-        'templates/**/*', 'images/**/*', 'js/**/*',
-        'css/**/*', '!app/**/*.ts'], { base : './' })
+  return gulp.src(['templates/**/*', 'images/**/*',
+        'css/**/*'], { base : './' })
     .pipe(gulp.dest('dist'))
 });
 
 // copy dependencies
-gulp.task('copy:libs', ['clean'], function() {
+gulp.task('bundle:vendor', ['clean'], function() {
   return gulp.src([
+      'js/jquery.js',
+      'js/foundation.js',
+      'js/what-input.js',
       'node_modules/core-js/client/shim.min.js',
       'node_modules/zone.js/dist/zone.js',
+      'node_modules/rxjs/bundles/Rx.js',
       'node_modules/reflect-metadata/Reflect.js',
       'node_modules/systemjs/dist/system.src.js',
-      'systemjs.config.js',
     ])
-    .pipe(gulp.dest('dist/lib'))
-});
-
-gulp.task('copy:node_modules', ['clean'], function() {
-  return gulp.src([
-      'node_modules/@angular/**/*',
-      'node_modules/angular2-in-memory-web-api/**/*',
-      'node_modules/angular2-localstorage/**/*',
-      'node_modules/rxjs/**/*',
-      'node_modules/lodash/lodash.js',
-      'node_modules/moment/moment.js',
-      'node_modules/ng2-popover/**/*',
-    ],{ "base" : "." })
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(concat('vendors.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 });
 
@@ -71,15 +74,39 @@ gulp.task('tslint', function() {
     .pipe(tslint.report('verbose'));
 });
 
+gulp.task('html', ['bundle:app', 'bundle:vendor'], function() {
+  gulp.src('index.html')
+    .pipe(htmlreplace({
+      'vendor': 'vendors.min.js',
+      'app': 'bundle.min.js',
+    }))
+    .pipe(gulp.dest('dist'));
+});
 
 // TypeScript compile
 gulp.task('compile', ['clean'], function () {
   return gulp
     .src(tscConfig.files)
-    .pipe(sourcemaps.init())
+    .pipe(addsrc.append('config.prod.js'))
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(typescript(tscConfig.compilerOptions))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('bundle:app', ['compile'], function() {
+  var builder = new Builder('', 'systemjs.config.js');
+  return builder
+      .buildStatic('dist/app/main.js', 'dist/bundle.min.js', { minify: true, sourceMaps: true})
+      .catch(function(err) {
+          console.log('Build error');
+          console.log(err);
+      });
+});
+
+gulp.task('build:watch', ['sass', 'build'], function () {
+  gulp.watch(['app/**/*', 'index.html', 'templates/**/*'], ['build']);
+  gulp.watch(['sass/**/*.scss'], ['sass']);
 });
 
 // update the tsconfig files based on the glob pattern
@@ -104,10 +131,10 @@ gulp.task('serve', ['build'], function() {
     // browser: ["google chrome", "firefox"]
   });
 
-  gulp.watch(['app/**/*', 'index.html', 'templates/**/*', 'css/**/*'], ['buildAndReload']);
+  gulp.watch(['app/**/*', 'index.html', 'templates/**/*'], ['buildAndReload']);
+  gulp.watch(['sass/**/*.scss'], ['sass']);
 });
 
-// gulp.task('build', ['tslint', 'compile', 'copy:libs', 'copy:assets']);
-gulp.task('build', ['compile', 'copy:libs', 'copy:node_modules', 'copy:assets']);
+gulp.task('build', ['compile', 'copy:assets', 'bundle:vendor','bundle:app', 'html']);
 gulp.task('buildAndReload', ['build'], reload);
 gulp.task('default', ['build']);
