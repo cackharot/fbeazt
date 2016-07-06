88099487afade9608316a2f4862bbfdebb9feac8
cackharot <cackharot@gmail.com>
2016-07-06 14:00:54 +0530

    generate api url based on doc location

--- src/web/app/AppConfig.ts
+++ src/web/app/AppConfig.ts
@@ -1,9 +1,16 @@
 export class AppConfig {
-  static BASE_URL:string = "http://localhost:4000/api";
-  // static BASE_URL:string = "http://185.73.37.215:8080/api";
+  static BASE_URL:string = AppConfig.getBaseUrl();
   static STORES_URL:string = AppConfig.BASE_URL+"/stores";
   static STORE_URL:string = AppConfig.BASE_URL+"/store";
   static PRODUCTS_URL:string = AppConfig.BASE_URL+"/products";
   static PRODUCT_URL:string = AppConfig.BASE_URL+"/product";
   static ORDER_URL:string = AppConfig.BASE_URL+"/order";
+
+  static getBaseUrl(){
+    if (window.location.host.match(/localhost/)) {
+      return 'http://localhost:4000/api';
+    } else {
+      return 'http://foodbeazt.in/api/';
+    }
+  }
 }
