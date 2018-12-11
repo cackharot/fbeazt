7be61650d9a664b0a66c20a8a7a4e8b02449a2c9
cackharot <cackharot@gmail.com>
2018-12-10 19:36:25 -0500

    update logging

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export FLASK_ENV=${FLASK_ENV:-development}
 export FLASK_APP=src/foodbeazt.uwsgi
-flask run -p 4000
+export FOODBEAZT_LOG="$(pwd)/src/foodbeazt/logging-dev.yml"
+flask run -p 4000 -h '0.0.0.0'
--- src/foodbeazt/flogging.py
+++ src/foodbeazt/flogging.py
@@ -3,11 +3,12 @@ import logging.config
 import os
 import yaml
 
+
 def setup_logging():
-  log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'logging.yml'))
-  filename = os.environ.get('FOODBEAZT_LOG',log_file)
-  with open(filename) as f:
-    config = yaml.safe_load(f.read())
-    logging.config.dictConfig(config)
-  logger = logging.getLogger(__name__)
-  logger.info("Loading logging config file from %s" % filename)
+    log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), 'logging.yml'))
+    filename = os.environ.get('FOODBEAZT_LOG', log_file)
+    with open(filename) as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    logger = logging.getLogger(__name__)
+    logger.info("Loading logging config file from %s" % filename)
